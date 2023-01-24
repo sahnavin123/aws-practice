e8b150fbf0f030ccc56b89f3f3a86061ccaa63e7
sahnavin123 <sahnavin123@gmail.com>
2023-01-24 18:49:06 +0530

    another commit

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -12,7 +12,7 @@ export function ApiStack({ stack, app }) {
         permissions: [table],
         environment: {
           TABLE_NAME: table.tableName,
-          // STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+          STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
         },
       },
     },
@@ -22,7 +22,7 @@ export function ApiStack({ stack, app }) {
       "Get /notes": "functions/list.main",
       "PUT /notes/{id}": "functions/update.main",
       "DELETE /notes/{id}": "functions/delete.main",
-      // "POST /billing": "functions/billing.main",
+      "POST /billing": "functions/billing.main",
     },
   });
 
