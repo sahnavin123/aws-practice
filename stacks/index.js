05d3843c48fa35479cdc3957c9772bab8e5c3394
sahnavin123 <sahnavin123@gmail.com>
2023-01-24 16:00:15 +0530

    securing the api

--- stacks/index.js
+++ stacks/index.js
@@ -1,5 +1,6 @@
 import {StorageStack} from './StorageStack';
 import { ApiStack } from "./ApiStack";
+import { AuthStack } from './AuthStack';
 
 
 export default function main(app) {
@@ -10,5 +11,5 @@ export default function main(app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack).stack(ApiStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
 }
